175ae6384d4a9dcc71422cdc7fe3093ceece529a
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-20 00:00:50 -0500

    Creacion del mapper del usuario

--- src/main/java/org/izpo/apigestionfacturas/mapper/UserMapper.java
+++ src/main/java/org/izpo/apigestionfacturas/mapper/UserMapper.java
@@ -1,4 +1,30 @@
 package org.izpo.apigestionfacturas.mapper;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.izpo.apigestionfacturas.model.dto.UserResponseDTO;
+import org.izpo.apigestionfacturas.model.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@AllArgsConstructor
 public class UserMapper {
+    private final ModelMapper modelMapper;
+
+    public User convertToEntity(UserResponseDTO userResponseDTO){
+        return modelMapper.map(userResponseDTO, User.class);
+    }
+    public UserResponseDTO convertToDTO(User user){
+        return modelMapper.map(user, UserResponseDTO.class);
+    }
+    public List<UserResponseDTO> convertToListDTO(List<User> users){
+        return users.stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
 }
