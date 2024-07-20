52558c878b1f2a7f8d59122833a453440de14287
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-19 23:15:07 -0500

    Primer Commit

---
+++ src/main/java/org/izpo/apigestionfacturas/ApiGestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package org.izpo.apigestionfacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionFacturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionFacturasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/izpo/apigestionfacturas/ApiGestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.izpo.apigestionfacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionFacturasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
